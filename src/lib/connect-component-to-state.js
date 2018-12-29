a011b4aa692e2a63db4ad538f030c08161344528
Timothy Robinson <timmyrobby@gmail.com>
2018-12-29 11:58:42 +0000

    Update connectComponentToState with added actions

--- src/lib/connect-component-to-state.js
+++ src/lib/connect-component-to-state.js
@@ -4,18 +4,27 @@ import * as React from 'react'
 import { connect } from 'react-redux'
 import type { Dispatch as DispatchType } from 'redux'
 import {
-  API_INITIALISE
+  API_INITIALISE,
+  FETCH_COLUMN_STATISTICS,
+  HIDE_NOTIFICATION
 } from '../redux/constants'
 
 function mapStateToProps (state: State): State {
   return {
-    loading: state.loading
+    loading: state.loading,
+    statisticsData: state.statisticsData,
+    omittedResultCount: state.omittedResultCount,
+    field: state.field,
+    columns: state.columns,
+    notification: state.notification
   }
 }
 
 function mapDispatchToProps (dispatch: DispatchType) {
   return {
-    initialiseData: () => dispatch({ type: API_INITIALISE })
+    initialiseData: () => dispatch({ type: API_INITIALISE }),
+    fetchColumnStatistics: (columnName: string) => dispatch({ type: FETCH_COLUMN_STATISTICS, columnName }),
+    dismissNotification: () => dispatch({ type: HIDE_NOTIFICATION })
   }
 }
 
